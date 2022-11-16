3bb16a652a826ab3f5c7445b7dad91ba773af12a
valmir <valmirchagas88@gmail.com>
2022-11-16 18:52:58 -0300

    msc finished

--- src/controller/LoginController.ts
+++ src/controller/LoginController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import LoginService from '../services/LoginService';
+import LoginService from '../service/LoginService';
 
 export default class LoginController {
   private service: LoginService;
